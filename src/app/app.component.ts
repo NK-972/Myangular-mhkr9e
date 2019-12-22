e49e9fda4f5c53e471f1a63f6c24cb4801194746
NK-972 <nicolascastry@gmail.com>
2019-12-22 16:54:41 +0100

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+interface Produit{
+  Prix : Number
+  , RD : Number
+  , Installations: string[]
+  , InstallationSelected: string
+  , Production: Number
+  , Qualite: Number
+  , IP: Number
+  , IT: Number
+  , Notoriete: Number
+  , Demande: Number
+  , Stock: Number
+  , MB: Number
+  , TMB: Number
+}
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
+
 export class AppComponent  {
+  @Input() data: JSON;
   name = 'Angular';
+  produit: Produit;
+  ngOnInit() {
+    this.produit ={
+      Prix : Number()
+      , RD : Number()
+      , Installations: []
+      , InstallationSelected: ''
+      , Production: Number()
+      , Qualite: Number()
+      , IP: Number()
+      , IT: Number()
+      , Notoriete: Number()
+      , Demande: Number()
+      , Stock: Number()
+      , MB: Number()
+      , TMB: Number() 
+    }
+  }
 }
