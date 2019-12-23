cbe55a12164c46e9adb360501802260874fa4733
NK-972 <nicolascastry@gmail.com>
2019-12-23 19:39:39 +0100

    1 file modified

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -7,15 +7,22 @@ import { Component, Input } from '@angular/core';
 })
 export class HelloComponent  {
   @Input() name: string;
+  produits: string = "[[JSON, tour, nom du produit, recette, prix, r&d, productio, production par installations, installation, actif, qualité, indice technique, image prix, marge brute, taux de marge brute, Demande, Stock, prix de mp, notoriété, indice r&d, breveter, Nombre de produit vendu, Frais de transport], [Bouteille d'eau 0, 0, Bouteille d'eau, 21811, 0, 0, 0, ,0,0,, ,0,1,, 0, 0, 0.000000, 1.00, 0, 0.00, null, null, null, 0.00, 1.00, 0, null, null], [Bouteille d'eau 1, 1, Bouteille d'eau, 21811, 30, 30000, 0, ,0,0,, ,0,1,, 1, 0, 0.000000, 1.00, 24, 4.00, 11200, 0, null, 0.00, 1.60, 0, 0, 0], [Bouteille d'eau 2, 2, Bouteille d'eau, 21811, 30, 30000, 2000, ,0,2000,, ,0,1,, 1, 0, 0.000000, 1.00, 19, 1.73, 9200, 0, null, 0.00, 1.60, 0, 2000, 8000], [Bouteille d'eau 3, 3, Bouteille d'eau, 21811, 30, 30000, 0, ,0,0,, ,0,1,, 1, 0, 0.000000, 1.00, 9, 0.43, 11200, 0, null, 0.00, 1.60, 0, 0, 0], [Bouteille d'eau 4, 4, Bouteille d'eau, 21811, 30, 30000, null, ,0,0,, ,0,1,, 1, 0, null, null, null, null, null, null, null, null, null, 0, null, null]]";
+  json_produits: JSON;
 
+   ngOnInit() {
+      this.StringToTable(this.produits);
+   }
 
   public StringToTable(str:String){
     let sortie: Array<string>;
-    let table: Array<Array<string>>;
+    var JSONQury = {};
     sortie = str.replace("[[", "").replace("]]", "").split("], [");
     sortie.forEach(function (value) {
-      table.push(value.split(", "));
+      console.log(value.split(", "));
+      JSONQury[value.split(", ")[0]] = value.split(", ");
     })
-    
+    this.json_produits = JSONQury as JSON;
+    console.log(this.json_produits);
   }
 }
