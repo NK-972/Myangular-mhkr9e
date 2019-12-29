b5bedbc504ca4c10dc21ad9c32bd4a67b2cb132d
NK-972 <nicolascastry@gmail.com>
2019-12-29 16:12:05 +0100

    4 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ProduitComponent } from './produit/produit.component';
+import { ProduitsComponent } from './produits/produits.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, ProduitComponent ],
+  declarations: [ AppComponent, ProduitComponent, ProduitsComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
