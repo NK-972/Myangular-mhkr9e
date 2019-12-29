2432f72e7d06700df7691599539c8dee5a12583c
NK-972 <nicolascastry@gmail.com>
2019-12-29 16:34:33 +0100

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ProduitComponent } from './produits/produit/produit.component';
+import { ProduitComponent } from './produit/produit.component';
 import { ProduitsComponent } from './produits/produits.component';
 
 @NgModule({
